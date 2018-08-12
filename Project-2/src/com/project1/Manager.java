3b0ec37e4a066e55c7c113f2044d2c4d5b701d75
arungupta436 <arun.gupta436@gmail.com>
2018-08-12 14:37:33 +0530

    LOCAL TO REMOTE

--- Project-2/src/com/project1/Manager.java
+++ Project-2/src/com/project1/Manager.java
@@ -1,9 +1,9 @@
-
-
+package com.project1;
 public class Manager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("My  Nama is Sanam");
 		System.out.println("My  Nama is Sanam");
 
 	}
